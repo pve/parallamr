c6476ccd457b33efe7d231b41e9e2b0d3e726090
Peter van Eijk <peter@peterhjvaneijk.nl>
2025-10-04 21:32:25 +0200

    update author.

--- src/parallamr/__init__.py
+++ src/parallamr/__init__.py
@@ -1,7 +1,7 @@
 """Parallamr: A command-line tool for running systematic experiments across multiple LLM providers."""
 
 __version__ = "0.2.0"
-__author__ = "HiveMind Collective"
+__author__ = "Peter's HiveMind Collective"
 __email__ = "contact@parallamr.dev"
 
 from .models import Experiment, ExperimentResult, ProviderResponse
